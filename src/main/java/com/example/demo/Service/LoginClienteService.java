460815d68ab665c527d0d4626c8df7a3913786fb
pedrootsugua <pedro.zanele@gmail.com>
2024-10-21 02:21:58 -0300

    feature: login perfil de cliente

--- src/main/java/com/example/demo/Service/LoginClienteService.java
+++ src/main/java/com/example/demo/Service/LoginClienteService.java
@@ -2,8 +2,12 @@ package com.example.demo.Service;
 
 import com.example.demo.DTO.AutenticacaoLoginDTO;
 import com.example.demo.DTO.CredencialDTO;
+import com.example.demo.Model.ClienteModel;
+import com.example.demo.Model.CredencialClienteModel;
 import com.example.demo.Model.CredencialModel;
 import com.example.demo.Model.UsuarioModel;
+import com.example.demo.Repository.ClienteRepository;
+import com.example.demo.Repository.CredencialClienteRepository;
 import com.example.demo.Repository.CredencialRepository;
 import com.example.demo.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,24 +16,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LoginService {
+public class LoginClienteService {
 
     @Autowired
-    private CredencialRepository credencialRepository;
+    private CredencialClienteRepository credencialClienteRepository;
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private ClienteRepository clienteRepository;
 
     @Autowired
     private PasswordService passwordService;
 
-    public ResponseEntity<AutenticacaoLoginDTO> entrar(CredencialDTO credencialDTO) {
-        CredencialModel credencial = credencialRepository.buscaPorEmail(credencialDTO.getEmail());
+    public ResponseEntity<AutenticacaoLoginDTO> entrarCliente (CredencialDTO credencialDTO) {
+        CredencialClienteModel credencial = credencialClienteRepository.buscaPorEmail(credencialDTO.getEmail());
         if (credencial != null) {
             boolean senhaCorreta = passwordService.verificarSenha(credencialDTO.getSenha(), credencial.getSenha());
             if (senhaCorreta) {
-                UsuarioModel usuario = usuarioRepository.buscarPorId(credencial.getId());
-                return new ResponseEntity<>(new AutenticacaoLoginDTO(true, usuario.getId(), usuario.getNome(), usuario.getGrupo(), usuario.isAtivo()), HttpStatus.OK);
+                ClienteModel cliente = clienteRepository.buscarPorId(credencial.getId());
+                return new ResponseEntity<>(new AutenticacaoLoginDTO(true, cliente.getId(), cliente.getNome()), HttpStatus.OK);
             }
         }
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
